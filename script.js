ac5fb2620a300239e523fbf81ae93f9ff12bac48
KJasas <karolis.jasass@gmail.com>
2017-04-26 10:12:16 +0300

    a

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-$(document).ready(function() {
-   $('.naver').mouseenter(function() {
-       $(this).animate({
-           width: '+=20px'
-       });
-	   $(this).animate({
-           color: 'black'
-       });
-   });
-   $('.naver').mouseleave(function() {
-       $(this).animate({
-           width: '-=20px'
-       }); 
-   });
+$(document).ready(function(){
+  
+  var menu = $(".menu");
+  var hamburger = $(".hamburger");
+  var line = $(".line");
+  var plyta = $(".tile");
+  var menuOpen;
+  
+  function openMenu(){
+    menu.css("left", "0px");
+    line.css("background", "#FFF");
+    menuOpen = true;
+  }
+  
+  function closeMenu(){
+    menu.css("left", "-320px");
+    line.css("background", "#991a1a");
+    menuOpen = false;
+  }
+  
+  function toggleMenu(){
+    if (menuOpen) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  }
+ 
+  hamburger.on({
+    mouseenter: function(){
+      openMenu();
+    }
+  });
+  
+  menu.on({
+    mouseleave: function(){
+      closeMenu();
+    }
+    
+  });
+  
+  hamburger.on({
+    click: function(){
+      toggleMenu();
+    }
+  })
+  
+  
+
 });
